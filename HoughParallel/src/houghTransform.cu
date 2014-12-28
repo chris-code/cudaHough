13ccf15f9d5051cbabe7b322e237186b8b88bb2e
Christian Mielers <Christian.Mielers@rub.de>
2014-12-28 13:29:00 +0100

    Reformatted code, sobel convolution
    Code has been reformatted such that namespaces and class visibility modifiers
    create a new level of indentation.
    
    First approach at a parallelized variant of convolution. No shared memory is
    used so far. Implementation of convolution with sobel filters in preprocess
    is present, but untested. Calculating the gradient strength still needs to
    be done.

--- HoughParallel/src/houghTransform.cu
+++ HoughParallel/src/houghTransform.cu
@@ -1,14 +1,103 @@
 #include "houghTransform.h"
 
+// this method transforms a rgb-color image to an grayvalue image
+CImg<double> RGBToGrayValueImage(const CImg<double> &image) {
+	// initialize the gray value image
+	CImg<double> grayImg(image.width(), image.height(), 1, 1);
+
+	// iterate over the image
+	for (int i = 0; i < image.width(); i++)
+		for (int j = 0; j < image.height(); j++) {
+			// The gray value is calculated by the following formula: 0.21 R + 0.72 G + 0.07 B
+			grayImg(i, j, 0, 0) = 0.21 * image(i, j, 0, 0) + 0.72 * image(i, j, 0, 1) + 0.07 * image(i, j, 0, 2);
+		}
+
+	return grayImg;
+}
+
+#define assertCheck(arg) { errorCheck(arg, __FILE__, __LINE__); }
+void errorCheck(cudaError_t returnCode, char *file, long line) {
+	if (returnCode != cudaSuccess) {
+		std::cerr << cudaGetErrorString(returnCode) << " occured at " << line << " in file " << file << std::endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
+double * cImgToGPU(CImg<double> image) {
+	double *gpuImage;
+	assertCheck(cudaMalloc(&gpuImage, image.height() * image.width() * sizeof(double)));
+	assertCheck(
+			cudaMemcpy(gpuImage, image.data(), image.height() * image.width() * sizeof(double), cudaMemcpyHostToDevice));
+	return gpuImage;
+}
+
+__global__ void convolve(double *result, double *image, long imgWidth, long imgHeight, double *filter, long filWidth,
+		long filHeight, long filAnchorX, long filAnchorY) {
+	long x = blockIdx.x * blockDim.x + threadIdx.x;
+	long y = blockIdx.y * blockDim.y + threadIdx.y;
+
+	if (x < imgWidth && y < imgHeight) {
+		double value = 0;
+
+		for (long filX = 0; filX < filWidth; ++filX) {
+			long posImgX = ((x - filAnchorX + filX) + imgWidth) % imgWidth;
+			for (long filY = 0; filY < filHeight; ++filY) {
+				long posImgY = ((y - filAnchorY + filY) + imgHeight) % imgHeight;
+
+				value += image[posImgY * imgWidth + posImgX] * filter[filY * filWidth + filX];
+			}
+		}
+
+		image[y * imgWidth + x] = value;
+	}
+}
+
+double * computeGradientStrength(double *grayValueImage, long width, long height) {
+	double cpuSobelX[] = { 1, 0, -1, 2, 0, -2, 1, 0, -1 };
+	double cpuSobelY[] = { 1, 2, 1, 0, 0, 0, -1, -2, -1 };
+	double *sobelX;
+	double *sobelY;
+	assertCheck(cudaMalloc(&sobelX, 9 * sizeof(double)));
+	assertCheck(cudaMalloc(&sobelY, 9 * sizeof(double)));
+	assertCheck(cudaMemcpy(sobelX, cpuSobelX, 9 * sizeof(double), cudaMemcpyHostToDevice));
+	assertCheck(cudaMemcpy(sobelY, cpuSobelY, 9 * sizeof(double), cudaMemcpyHostToDevice));
+
+	double *gradientX;
+	double *gradientY;
+	assertCheck(cudaMalloc(&gradientX, width * height * sizeof(double)));
+	assertCheck(cudaMalloc(&gradientY, width * height * sizeof(double)));
+
+	dim3 threads(16, 16);
+	dim3 blocks((width + threads.x - 1) / threads.x, (height + threads.y - 1) / threads.y);
+	convolve<<<blocks, threads>>>(gradientX, grayValueImage, width, height, sobelX, 3, 3, 1, 1);
+	convolve<<<blocks, threads>>>(gradientY, grayValueImage, width, height, sobelY, 3, 3, 1, 1);
+
+	double *gradientStrength;
+	assertCheck(cudaMalloc(&gradientStrength, width * height * sizeof(double)));
+//	TODO calc. gradient strength
+
+	cudaFree(sobelX);
+	cudaFree(sobelY);
+	cudaFree(gradientX);
+	cudaFree(gradientY);
+
+	return gradientStrength;
+}
+
 CImg<bool> cudaHough::preprocess(CImg<double> image) {
-//	TODO
+	CImg<double> cpuGrayValueImage = RGBToGrayValueImage(image);
+	double *grayValueImage = cImgToGPU(cpuGrayValueImage);
+
+	double *gradientStrengthImage = computeGradientStrength(grayValueImage, image.width(), image.height());
+
+	cudaFree(grayValueImage);
 }
 
 CImg<long> cudaHough::transform(CImg<bool> binaryImage) {
 //	TODO
 }
 
-std::vector<std::pair<double, double> > cudaHough::extractMostLikelyLines(
-		CImg<long> accumulatorArray, long linesToExtract) {
+std::vector<std::pair<double, double> > cudaHough::extractMostLikelyLines(CImg<long> accumulatorArray,
+		long linesToExtract) {
 
 }
--- HoughParallel/src/houghTransform.h
+++ HoughParallel/src/houghTransform.h
@@ -1,34 +1,36 @@
 #pragma once
 
 #include <vector>
+#include <iostream>
 #include <CImg.h>
+#include <cuda_runtime.h>
 
 using namespace cimg_library;
 
 namespace cudaHough {
-CImg<bool> preprocess(CImg<double> image);
-CImg<long> transform(CImg<bool> binaryImage);
-std::vector< std::pair<double, double> > extractMostLikelyLines(CImg<long> accumulatorArray, long linesToExtract);
+	CImg<bool> preprocess(CImg<double> image);
+	CImg<long> transform(CImg<bool> binaryImage);
+	std::vector<std::pair<double, double> > extractMostLikelyLines(CImg<long> accumulatorArray, long linesToExtract);
 
-class HoughParameterSet {
-public:
-	HoughParameterSet(int width, int height) {
-		this->minTheta = 0;
-		this->maxTheta = cimg_library::cimg::PI;
-		this->stepsPerRadian = 57.295 * 2;
-		this->stepsPerPixel = 2;
-		this->minR = -sqrt(width * width + height * height);
-		this->maxR = -minR;
-	}
+	class HoughParameterSet {
+		public:
+			HoughParameterSet(int width, int height) {
+				this->minTheta = 0;
+				this->maxTheta = cimg_library::cimg::PI;
+				this->stepsPerRadian = 57.295 * 2;
+				this->stepsPerPixel = 2;
+				this->minR = -sqrt(width * width + height * height);
+				this->maxR = -minR;
+			}
 
-	virtual ~HoughParameterSet() {
-	}
+			virtual ~HoughParameterSet() {
+			}
 
-	double minTheta;
-	double maxTheta;
-	double stepsPerRadian;
-	double stepsPerPixel;
-	double minR;
-	double maxR;
-};
+			double minTheta;
+			double maxTheta;
+			double stepsPerRadian;
+			double stepsPerPixel;
+			double minR;
+			double maxR;
+	};
 }
